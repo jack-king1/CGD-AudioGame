1b1b030deaad4be96e9293088774f82a7b8991d8
a <a>
2019-11-20 13:47:11 +0000

    Wip Fog of war got Rep meeting

---
+++ CGD-AudioGame/Assets/Scripts/TempMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMove : MonoBehaviour
+{
+    float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        x *= speed * Time.deltaTime;
+        z *= speed * Time.deltaTime;
+
+        transform.Translate(x, 0, z);
+    }
+}
