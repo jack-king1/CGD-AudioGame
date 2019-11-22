1dd96508f8b1070031548e846f8212b15b14ad96
King <jack3.king@live.uwe.ac.uk>
2019-11-22 19:06:10 +0000

    Simple camera follow script

--- CGD-AudioGame/Assets/CameraFollow.cs
+++ CGD-AudioGame/Assets/CameraFollow.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject m_target;
-    [Range(0.1f,1.0f)]
+    [Range(0.01f,1.0f)]
     public float m_cameraSpeed;
 
-    [Range(1f, 10f)]
-    public float m_cameraZoomOffset;
+    [Range(1f, 20f)]
+    public float m_cameraZoomOffset = 5f;
 
     private void Start()
     {
@@ -23,9 +23,9 @@ public class CameraFollow : MonoBehaviour
             Vector3 followPosition = new Vector3(
                 m_target.transform.position.x, 
                 m_target.transform.position.y + m_cameraZoomOffset,
-                m_target.transform.position.z);
+                m_target.transform.position.z - 2.5f);
 
-            transform.position = Vector3.Lerp(transform.position, followPosition, 0.5f);
+            transform.position = Vector3.Slerp(transform.position, followPosition, m_cameraSpeed);
         }
         else
         {
