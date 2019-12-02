add4b3ac21c1760df9cacb78b426bba754b16185
King <jack3.king@live.uwe.ac.uk>
2019-12-02 02:11:32 +0000

    Sprucing up coin prefab

--- CGD-AudioGame/Assets/CoinCollect.cs
+++ CGD-AudioGame/Assets/CoinCollect.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class CoinCollect : MonoBehaviour
 { 
     public float scoreAmount = 100;
+    public Transform start;
+    public Transform end;
+
+    private void Update()
+    {
+        transform.Rotate(0, 60 * Time.deltaTime, 0);
+
+        transform.position = new Vector3(gameObject.transform.position.x, Mathf.Lerp(start.transform.position.y, end.transform.position.y, Mathf.PingPong(Time.time, 1)), gameObject.transform.position.z);
+    }
 
     void OnTriggerEnter(Collider col)
     {
