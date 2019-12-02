3b6c15ddb5a0a93e458367abf14b61106112599b
Hector <Hector1998@live.co.uk>
2019-12-02 00:23:03 +0000

    fix coincol

--- CGD-AudioGame/Assets/CoinCollect.cs
+++ CGD-AudioGame/Assets/CoinCollect.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class CoinCollect : MonoBehaviour
 {
-    private PlayerData Pd;
    
     void OnTriggerEnter(Collider col)
     {
         Debug.Log("Coin Collided");
         if (col.CompareTag("Player"))
         {
-            //Add player score here bois.
-            Pd.playerScore += 1.0f;
+            col.gameObject.GetComponent<PlayerData>();
 
             Destroy(gameObject);
         }
