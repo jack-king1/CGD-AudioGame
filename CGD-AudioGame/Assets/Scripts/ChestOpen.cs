1bcb58fa54ac35870ec52b4ab1baf509ec26832a
h <h>
2019-12-03 19:30:32 +0000

    position error

--- CGD-AudioGame/Assets/Scripts/ChestOpen.cs
+++ CGD-AudioGame/Assets/Scripts/ChestOpen.cs
@@ -10,14 +10,22 @@ public class ChestOpen : MonoBehaviour
     public bool animPlayed = false;
     public GameObject ps;
     public GameObject button;
+    public GameObject coin1;
+    public GameObject coin2;
+    public GameObject coin3;
 
     private int playerID;
 
     // Start is called before the first frame update
+    // posistion -60, -90 -120
     void Start()
     {
         anim = GetComponent<Animator>();
         playerID = GetComponent<PlayerData>().PlayerID();
+
+        coin1.SetActive(false);
+        coin2.SetActive(false);
+        coin3.SetActive(false);
     }
 
    
@@ -35,6 +43,10 @@ public class ChestOpen : MonoBehaviour
             {
                 
                 StartCoroutine (Chest());
+                //coin1.GetComponent<GameObject>().transform.rotation.y(-90);
+                coin1 = Instantiate(coin1, transform.TransformPoint(Vector3.forward), transform.SetPositionAndRotation(new Vector3(0,0,0), Quaternion.Euler(new Vector3(0,-90,0)));
+
+
             }
         }
     }
@@ -54,5 +66,9 @@ public class ChestOpen : MonoBehaviour
         button.SetActive(false);
         yield return new WaitForSeconds(2.1f);
         ps.SetActive(true);
+        coin1.SetActive(true);
+        coin2.SetActive(true);
+        coin3.SetActive(true);
+        anim.Play("CoinBounce");
     }
 }
