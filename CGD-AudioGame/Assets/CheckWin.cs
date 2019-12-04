cddd19dfac961e18594685622d49249e12456ebe
Jack <jack3.king@live.uwe.ac.uk>
2019-12-04 11:02:15 +0000

    Last Minute Tweaks

---
+++ CGD-AudioGame/Assets/CheckWin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckWin : MonoBehaviour
+{
+    public GameObject[] Chests;
+    private LevelManager lm;
+
+    private void Start()
+    {
+        lm = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
+    }
+
+
+    private void Update()
+    {
+        int animPlayed = 0;
+        foreach(var chest in Chests)
+        {
+            if(chest.GetComponent<ChestOpen>().animPlayed)
+            {
+                animPlayed++;
+            }
+        }
+
+        if(animPlayed == 2)
+        {
+            lm.LevelWin();
+        }
+    }
+}
